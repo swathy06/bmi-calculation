a363a56f352379bbba076b4d664b462851f9d03e
swathy06 <38415961+swathy06@users.noreply.github.com>
2018-07-12 10:15:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -155,4 +155,5 @@ if __name__ == '__main__':
  
     print("Starting app on port %d" % port)
  
-    app.run(debug=False, port=port, host='0.0.0.0')
+    #app.run(debug=False, port=port, host='0.0.0.0')
+    app.run()
