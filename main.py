b9d96a7ddd384661985cfadcac6ec3347bda1f95
swathy06 <38415961+swathy06@users.noreply.github.com>
2018-07-13 14:26:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,21 +36,10 @@ def webhook():
 def processRequest(req):
     print("Request:")
     print(json.dumps(req, indent=4))
-    if req.get("result").get("action") == "yahooWeatherForecast":
-        baseurl = "https://query.yahooapis.com/v1/public/yql?"
-        yql_query = makeYqlQuery(req)
-        if yql_query is None:
-            return {}
-        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-        result = urlopen(yql_url).read()
-        data = json.loads(result)
-        res = makeWebhookResult(data)
-    elif req.get("result").get("action") == "getBmi":
+   
+    if req.get("result").get("action") == "getBmi":
         data = req
         res = makeWebhookResultForGetBmi(data)
-    elif req.get("result").get("action") == "getChemicalSymbol":
-        data = req
-        res = makeWebhookResultForGetChemicalSymbol(data)
     else:
         return {}
     return res
